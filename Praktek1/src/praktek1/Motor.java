9bdb92bea5a04a08381b598caa10e2339edf1832
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-03-27 20:55:37 +0700

    Membuat atribut dan behavior

--- Praktek1/src/praktek1/Motor.java
+++ Praktek1/src/praktek1/Motor.java
@@ -10,5 +10,18 @@ package praktek1;
  * @author Ajie
  */
 public class Motor {
+    double tahun_pembuatan;
+    String warna;
+    String shock;
+    String merk_ban;
+    double kapasitas_tangki;
+    
+    void mengisi_bensin (){
+    System.out.println("tahun_pembuatan :"+tahun_pembuatan);
+    System.out.println("warna :"+warna);
+    System.out.println("shock :"+shock);
+    System.out.println("merk_ban");
+    System.out.println("kapasitas_tangki :"+kapasitas_tangki);
+            }
     
 }
